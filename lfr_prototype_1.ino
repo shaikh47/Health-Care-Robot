4d065cddb7abd53c0c313e756595d5149b056c5f
shaikh47 <57007890+shaikh47@users.noreply.github.com>
2020-05-12 22:42:22 +0600

    Add files via upload
    added path tracker, tracks the path it went. also pid is recalibrated

--- lfr_prototype_1.ino
+++ lfr_prototype_1.ino
@@ -5,12 +5,12 @@
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 
-#define Kp 0.05 //  0.7 works okay   0.05 also works good  
+#define Kp 0.01 //  0.01 works okay   0.05 works good  
 #define Kd 0.39 //  0.39
 #define Ki 0.06 // 0.06
 
-#define MaxSpeed 40 // max speed of the robot (50)
-#define BaseSpeed 35 // this is the speed at which the motors should spin when the robot is perfectly on the line  (50)
+#define MaxSpeed 40 // max speed of the robot (40)
+#define BaseSpeed 35 // this is the speed at which the motors should spin when the robot is perfectly on the line  (35)
 #define NUM_SENSORS 10 // number of sensors used
 
 #define speedturn 180
@@ -29,6 +29,8 @@ void wait(void);
 String str;
 char data = 0;
 const byte interruptPin = 2;
+
+String path="";
 
 int x = 30;
 int y = 255;
@@ -70,18 +72,7 @@ void setup() {
 
   // configure the sensors
   qtr.setTypeRC();
-  qtr.setSensorPins((const uint8_t[]) {
-    35,
-    37,
-    39,
-    41,
-    43,
-    45,
-    47,
-    49,
-    51,
-    53
-  }, SensorCount); //53,51,49,47,45,43,41,39,37,35
+  qtr.setSensorPins((const uint8_t[]) {35,37,39,41,43,45,47,49,51,53}, SensorCount); //53,51,49,47,45,43,41,39,37,35
   //qtr.setEmitterPin(2);                                                             //35,37,39,41,43,45,47,49,51,53
 
   lcd.setCursor(0,0); //we start writing from the first row first column
@@ -167,9 +158,49 @@ void leftHand(){
     wait(); //wait a bit
     position = qtr.readLineBlack(sensorValues); //read the line again
     delay(100); //wait for 100 ms
+        //below if handles the all black sensor situation which signifies the start position
+    //paste
+    if((sensorValues[0] > 100 && sensorValues[9] > 100) || (sensorValues[1] > 100 && sensorValues[8] > 100))
+    {
+        wait();  //just wait for now
+        delay(1000);
+        String pathTwo="";
+          //display the path array
+            lcd.clear();
+            lcd.setCursor(0,0); 
+            lcd.print(path);
+            if(path.length()>16){
+              pathTwo=path.substring(16);
+              lcd.setCursor(1,1);
+              lcd.print(pathTwo);
+            }  
+            while(1){
+              
+            }
+    }
+    //paste
+    //handling the case where we see a T intersection
+    //below if handles the situation where we see no line after a T intersection
+    else if (sensorValues[0] < 200 && sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200 && sensorValues[8] < 200 && sensorValues[9] < 200) 
+    { //0000000000
+      path=path+"L";
+      lcd.clear();
+      lcd.setCursor(0,0); 
+      lcd.print("T INTER");  
+      wait();
+      delay(1000);
+    
+      position = qtr.readLineBlack(sensorValues);
+        while (sensorValues[3] < 150 || sensorValues[4] < 150) {   //move left while the bot finds the line
+        position = qtr.readLineBlack(sensorValues);
+        hardleft();
+        }
+      wait();
+      delay(100);
+    }
     //now check here to see if these is a straight line in order to verify if as a plus intersection
-    if (!(sensorValues[0]<200 && sensorValues[1]<200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200 && sensorValues[8]<200 && sensorValues[9]<200)){ //111000
-      
+    else if (!(sensorValues[0]<200 && sensorValues[1]<200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200 && sensorValues[8]<200 && sensorValues[9]<200)){ //111000
+      path=path+"L";
       lcd.clear();
       lcd.setCursor(0,0); 
       lcd.print("PLUS INTER");
@@ -190,35 +221,12 @@ void leftHand(){
       wait();
       delay(500);
     }
-    //handling the case where we see a T intersection
-    //below if handles the situation where we see no line after a T intersection
-    else if (sensorValues[0] < 200 && sensorValues[1] < 200 && sensorValues[2] < 200 && sensorValues[3] < 200 && sensorValues[4] < 200 && sensorValues[5] < 200 && sensorValues[6] < 200 && sensorValues[7] < 200 && sensorValues[8] < 200 && sensorValues[9] < 200) 
-    { //0000000000
-      lcd.clear();
-      lcd.setCursor(0,0); 
-      lcd.print("T INTER");  
-      wait();
-      delay(1000);
-    
-      position = qtr.readLineBlack(sensorValues);
-        while (sensorValues[3] < 150 || sensorValues[4] < 150) {   //move left while the bot finds the line
-        position = qtr.readLineBlack(sensorValues);
-        hardleft();
-        }
-      wait();
-      delay(100);
-    }
-    //below if handles the all black sensor situation which signifies the start position
-    else if(sensorValues[0] > 100 && sensorValues[1] > 100 && sensorValues[2] > 100 && sensorValues[3] > 100 && sensorValues[4] > 100 && sensorValues[5] > 100 && sensorValues[6] > 100 && sensorValues[7] > 100 && sensorValues[8] > 100 && sensorValues[9] > 100)
-    {
-        wait();  //just wait for now
-        delay(1000);
-    }
   }//end of black line condition
 
   //handling the case for no line. when the robot see's no line(case 0)
   else if (sensorValues[0] < 100 && sensorValues[1] < 100 && sensorValues[2] < 100 && sensorValues[3] < 100 && sensorValues[4] < 100 && sensorValues[5] < 100 && sensorValues[6] < 100 && sensorValues[7] < 100 && sensorValues[8] < 100 && sensorValues[9] < 100)
   {
+    path=path+"B";
     lcd.clear();
     lcd.setCursor(0,0); 
     lcd.print("ROTATE NO LINE");  
@@ -241,21 +249,14 @@ void leftHand(){
   //handling case 4 (-|  like symbol  and  case 2 (⌝ like section)) no matter what we have to go left
    else if(sensorValues[9] > 900 && sensorValues[0] < 200 && (sensorValues[5] > 900 || sensorValues[6] > 900))   //400
         {
+            path=path+"L";
             lcd.clear();
             lcd.setCursor(0,0); 
             lcd.print("-|  or left turn");  
             wait();
             delay(1000);
           
-            wait();
-            delay(500);
-            count=0;
-            while(count<4){
-              forward();
-            }
-            wait();
-            delay(100);
-            steps_forward(15);
+            steps_forward(19);
             wait();
             delay(500);
             
@@ -283,21 +284,14 @@ void leftHand(){
             wait();
             delay(1000);
           
-            wait();
-            delay(500);
-            count=0;
-            while(count<4){
-              forward();
-            }
-            wait();
-            delay(100);
-            steps_forward(15);
+            steps_forward(19);
             wait();
             delay(500);
             
             position = qtr.readLineBlack(sensorValues);
             //if there is no line we must go right
             if(sensorValues[0] < 100 && sensorValues[1] < 100 && sensorValues[2] < 100 && sensorValues[3] < 100 && sensorValues[4] < 100 && sensorValues[5] < 100 && sensorValues[6] < 100 && sensorValues[7] < 100 && sensorValues[8] < 100 && sensorValues[9] < 100){
+                path=path+"R";
                 while (sensorValues[0] < 500 || sensorValues[1] < 500) {
                     position = qtr.readLineBlack(sensorValues);
                     hardright();
@@ -315,7 +309,7 @@ void leftHand(){
             //else, if there is line we must go forward
             else{  
                 //do nothing here the bot will go forward anyway
-                //this else is not even needed, remove this later
+                path=path+"F";
             }
   }
 }
@@ -367,8 +361,8 @@ void followLine() {
   if (sensorValues[0] < 100 && sensorValues[1] < 100 && sensorValues[2] < 100 && sensorValues[3] < 100 && sensorValues[4] < 100 && sensorValues[5] < 100 && sensorValues[6] < 100 && sensorValues[7] < 100 && sensorValues[8] < 100 && sensorValues[9] < 100){
     forward();
   } else {
-    move(1, rightMotorSpeed, 1); //motor derecho hacia adelante
-    move(0, leftMotorSpeed, 1); //motor izquierdo hacia adelante
+      move(1, rightMotorSpeed, 1); //motor derecho hacia adelante
+      move(0, leftMotorSpeed, 1); //motor izquierdo hacia adelante
   }
 
   prev_position = position;
