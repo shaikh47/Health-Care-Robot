86fabc12cd7148529359239171b74ede8f0a4424
shaikh47 <57007890+shaikh47@users.noreply.github.com>
2020-08-27 21:03:42 +0600

    Add files via upload
    distance measure code for collision avoidance

---
+++ distanceMonitor/sharpIR.ino
@@ -0,0 +1,86 @@
+#include<Servo.h>
+#define pin A0
+
+uint16_t get_gp2d12 (uint16_t value);
+
+Servo servo;
+
+void setup () {
+    Serial.begin (9600);
+    pinMode (pin, INPUT);
+    servo.attach(11);
+    pinMode(13,OUTPUT);
+}
+
+void loop () {
+    int pos;
+    uint16_t value;
+    uint16_t range;
+
+    for (pos = 30; pos <= 110; pos += 1) {
+      servo.write(pos); 
+      value = analogRead (pin);
+      range = get_gp2d12(value)/10.0;
+      
+      Serial.print("Actual range: ");
+      Serial.print(range);
+      Serial.print("Predicted range: ");
+      Serial.print(distanceCalc(pos));
+      Serial.println();
+      checkCollision(range,distanceCalc(pos));
+           
+      delay(20);                     
+    }
+  for (pos = 110; pos >= 30; pos -= 1) { 
+      servo.write(pos); 
+      value = analogRead (pin);
+      range = get_gp2d12(value)/10.0;
+      
+      Serial.print("Actual range: ");
+      Serial.print(range);
+      Serial.print("Predicted range: ");
+      Serial.print(distanceCalc(pos));
+      Serial.println();
+      checkCollision(range,distanceCalc(pos));
+      
+      delay(20);                       
+    }
+}
+
+uint16_t get_gp2d12 (uint16_t value) {
+    if (value < 10) value = 10;
+    return ((67870.0 / (value - 3.0)) - 40.0);
+}
+
+
+void checkCollision(double senseDict,double predictDist){
+  int var=analogRead(pin);
+  int dist=get_gp2d12(var)/10.0;
+
+  if(senseDict<predictDist){
+    while(dist<predictDist){
+      var=analogRead(pin);
+      dist=get_gp2d12(var)/10.0;
+      digitalWrite(13,HIGH);
+      
+      Serial.print(dist);
+      Serial.print("   ");
+      Serial.print(predictDist);
+      Serial.println("");
+    }
+  }
+  digitalWrite(13,LOW);
+}
+
+
+double distanceCalc(double angleServo){
+  int definedDist=15;//defined distance value at 0 degree angle
+  
+  double ang=map(angleServo,30,110,20,155);
+ 
+  ang=abs(90-ang);
+  ang=ang*(PI/180.0);
+ 
+  double predictDist=(double)definedDist/sin(90-ang);
+  return predictDist;
+}
