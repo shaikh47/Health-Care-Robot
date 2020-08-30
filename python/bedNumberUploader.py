4f9c11f84fafcae5947fa0fbfd57b455c79d220c
shaikh47 <57007890+shaikh47@users.noreply.github.com>
2020-08-30 19:21:21 +0600

    Update bedNumberUploader.py
    same task done with pyrebase library. code written for windows. for linux just have to change the COM port names.

--- python/bedNumberUploader.py
+++ python/bedNumberUploader.py
@@ -3,11 +3,21 @@ import time
 from random import randint
 import csv
 import turtle
-from firebase import firebase
+import pyrebase
 import json
 from datetime import datetime
 
-firebase = firebase.FirebaseApplication("https://iot-webapplication-47.firebaseio.com/");
+config={"apiKey": "AIzaSyDLEco45i2BszWHJb67eAc7qx8OZ-d9vC8",
+        "authDomain": "iot-webapplication-47.firebaseapp.com",
+        "databaseURL": "https://iot-webapplication-47.firebaseio.com",
+        "projectId": "iot-webapplication-47",
+        "storageBucket": "iot-webapplication-47.appspot.com",
+        "messagingSenderId": "639567912933",
+        "appId": "1:639567912933:web:e8140919d9641904e2590c"}
+
+firebase = pyrebase.initialize_app(config)
+db = firebase.database()
+
 ser=serial.Serial('COM4',baudrate=9600,timeout=1)
 time.sleep(3)
 
@@ -26,10 +36,10 @@ while 1:
 
           strTemp = temp.split('$')  # inserts the splitted words in a list
           for i in strTemp:
-                data={'name':'blank'}
+                data={'test':'data'}
                 dataToSend='BED NUMBER_'+i
                 path='/main/'+dataToSend+'/'
-                result=firebase.put(path, "test","blank")
+                result=db.child("main").child(dataToSend).set(data)
                 print(dataToSend)
       print(temp.strip())
       temp=''
