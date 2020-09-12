ddbb25f51614fb51ec2156d2457984882a6c0c23
shaikh47 <57007890+shaikh47@users.noreply.github.com>
2020-09-12 20:20:30 +0600

    Add files via upload

--- finalYearProjectWebPage/medicinePage/saveData.js
+++ finalYearProjectWebPage/medicinePage/saveData.js
@@ -239,3 +239,116 @@ btnSlotSubmit.addEventListener('click', (e) => { //alternate saving
        document.getElementById("slotDiplay").innerHTML = "CURRENT SELECTED SLOT: "+slotNumber;
     });
 });
+
+
+var values=[];
+var maxValues=[];
+var minValues=[];
+var avgValues=[];
+var variable=[];
+var timeValues=[];
+var from;
+var counter=0;
+
+
+$(document).ready(function() {
+    var database = firebase.database();
+    
+var rootRef=firebase.database().ref().child("main/"+site+"/vitals/dateData/");
+    rootRef.on("child_added", snap => {
+        var childData = snap.val();
+        //console.log(name);
+        timeValues.push(childData);
+        //$(".max").text("max: " + maxValues);
+    });  
+
+var rootRef=firebase.database().ref().child("main/"+site+"/vitals/heartRate/");
+    rootRef.on("child_added", snap => {
+        var childData = snap.val();
+        //console.log(name);
+        minValues.push(childData);
+       // $(".min").text("min: " + minValues);
+        //
+        var ctx = document.getElementById('chartOfHeartRate').getContext('2d');
+            var myChart = new Chart(ctx, { //change should go here
+                type: 'line',
+                data: {
+                    labels: timeValues, //x axis value
+                    datasets: [
+                        {
+                           label: 'HEART RATE READINGS', //y axis value's label
+                        data: minValues, //y axis values [can be put an array]
+                        backgroundColor: [
+                              //color of avg curve
+                            'rgba(52, 235, 85, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1 
+                        }       
+                              ]
+                },
+                options: {
+                    animation: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+    });  
+    var rootRef=firebase.database().ref().child("main/"+site+"/vitals/temperature/");
+    rootRef.on("child_added", snap => {
+        var childData = snap.val();
+        //console.log(name);
+        maxValues.push(childData);
+       // $(".min").text("min: " + minValues);
+        //
+        var ctx = document.getElementById('chartOfTemperature').getContext('2d');
+            var myChart = new Chart(ctx, { //change should go here
+                type: 'line',
+                data: {
+                    labels: timeValues, //x axis value
+                    datasets: [
+                        {
+                           label: 'TEMPERATURE READINGS', //y axis value's label
+                        data: maxValues, //y axis values [can be put an array]
+                        backgroundColor: [
+                              //color of avg curve
+                            'rgba(3, 240, 252, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1 
+                        }       
+                              ]
+                },
+                options: {
+                    animation: false,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+    }); 
+});
+
